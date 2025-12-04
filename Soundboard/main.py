cfb0f79e66deb8507c73380849095941014b54fe
7340796 <t.w.westland@students.uu.nl>
2025-12-04 19:01:42 +0100

    waa

--- Soundboard/main.py
+++ Soundboard/main.py
@@ -1,7 +1,19 @@
 from pynput import keyboard
-import soundboard
+import os
+import soundfile as sf
+import sounddevice as sd
+import numpy as np
 
+d = ["CABLE Input (VB-Audio Virtual Cable)", "Luidsprekers (Realtek(R) Audio)", ]
 pressedKeys = set({}) # stores all held down keys, for implementing key click, not key hold stuff
+def play():
+    # create numpy array with audio data
+    device = "Luidsprekers (Realtek (R) Audio) WASAPI"
+    path = os.path.join(os.getcwd(), "sounds", "toink.wav")
+    data, samplerate = sf.read(path)
+    device_info = sd.query_devices(device)
+    device_samplerate = device_info['default_samplerate']
+    sd.play(data, device_samplerate, device = device)
 
 def on_press(key, injected):
     if key in pressedKeys:  # key is already being pressed 
@@ -14,8 +26,8 @@ def on_press(key, injected):
         return False
     
     # trigger sound
-    soundboard.play(key)
-
+    if key == keyboard.Key.space:
+        play()
 
 def on_release(key, injected):
     pressedKeys.discard(key)
@@ -24,5 +36,4 @@ def on_release(key, injected):
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
-    soundboard.initialize()
     listener.join()
